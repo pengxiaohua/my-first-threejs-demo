da44f3a9755daa52582236dc45c71803c5a23132
Xiaohua Peng <609370075@qq.com>
2022-11-03 22:40:49 +0800

    feat: 增加立方体demo

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
 import './index.css'
+
+const Box = () => (
+  <mesh castShadow>
+    <boxGeometry />
+    <meshStandardMaterial color="#1890FF" />
+  </mesh>
+)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} />
+      <OrbitControls makeDefault />
+      <Box />
+    </Canvas>
   </React.StrictMode>
 )
