21186bd679de32a448d790173f7b91100ceaff8d
Xiaohua Peng <609370075@qq.com>
2022-11-05 00:13:25 +0800

    feat: change plane styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
+import * as THREE from 'three'
 import { OrbitControls } from '@react-three/drei'
 
 import './index.css'
@@ -12,9 +13,20 @@ const Box = () => (
   </mesh>
 )
 
+// 定义欧拉角，描述在三维坐标系的方向
+const euler = new THREE.Euler(-Math.PI / 2, 0, 0)
+// 设置立方体的三维向量位置坐标值
+const position = new THREE.Vector3(0, -0.5, 0)
+
+// 定义一个地平面
 const Plane = () => (
-  <mesh receiveShadow>
-    <planeGeometry />
+  <mesh
+    receiveShadow
+    rotation={euler}
+    position={position}
+  >
+    {/* 设置水平面及其尺寸 */}
+    <planeGeometry args={[10, 20]} />
     <meshStandardMaterial color={0xffffff} />
   </mesh>
 )
