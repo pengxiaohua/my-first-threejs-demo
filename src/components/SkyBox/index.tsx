a61e06c52dc1371d179344fa64772042a00c41e8
Xiaohua Peng <609370075@qq.com>
2022-11-20 22:43:42 +0800

    feat: adjust stars

--- src/components/SkyBox/index.tsx
+++ src/components/SkyBox/index.tsx
@@ -1,21 +1,31 @@
+import { useLayoutEffect } from 'react'
 import * as THREE from 'three'
-import { useTexture } from '@react-three/drei'
+import { useTexture, Stars } from '@react-three/drei'
 
 const SkyBox = () => {
 
     const galaxyTexture = useTexture('/public/texture/galaxy.jpg')
 
+    useLayoutEffect(() => {
+        galaxyTexture.wrapS = galaxyTexture.wrapT = THREE.MirroredRepeatWrapping
+        galaxyTexture.repeat.set(2, 2)
+    })
+
     return (
-        <mesh>
-            <sphereBufferGeometry args={[800, 32, 32]} />
-            <meshPhongMaterial
-                emissive={0xff2190}
-                side={THREE.BackSide}
-                // 发光的强度
-                emissiveIntensity={0.1}
-                map={galaxyTexture}
-            />
-        </mesh>
+        <>
+            <Stars factor={30} fade speed={2} depth={50} radius={280} count={6000} saturation={0} />
+            <mesh>
+                <sphereBufferGeometry args={[800, 32, 32]} />
+                <meshPhongMaterial 
+                    emissive={0xff2190}
+                    side={THREE.BackSide}
+                    // 发光的强度
+                    emissiveIntensity={0.1}
+                    map={galaxyTexture}
+                />
+            </mesh>
+        </>
+
     )
 }
 
