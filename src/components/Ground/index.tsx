51349ff51d172b265f70a8fcbe9b1ca5ab82fa20
Xiaohua Peng <609370075@qq.com>
2022-11-24 23:19:54 +0800

    feat: update Ground

--- src/components/Ground/index.tsx
+++ src/components/Ground/index.tsx
@@ -1,9 +1,11 @@
+import * as THREE from 'three'
+
 import { planeSize } from "../../constant"
 
 const Ground = () => {
     return (
         <>
-            <mesh>
+            <mesh rotation={new THREE.Euler(-Math.PI / 2, 0, 0)}>
                 <planeBufferGeometry args={[planeSize, planeSize]} />
                 <meshStandardMaterial emissive={0xffffff} roughness={0} metalness={0} />
             </mesh>
